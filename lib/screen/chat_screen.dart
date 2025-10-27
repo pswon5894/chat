bb8e3e45c3bffeb1ed5fbb75dd18fafcf65899d2
pswon5894 <pswon5894@gmail.com>
2025-10-28 08:48:54 +0900

    재연결
    # Conflicts:
    #	lib/screen/chat_screen.dart

--- lib/screen/chat_screen.dart
+++ lib/screen/chat_screen.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/login_provider.dart';
@@ -15,86 +14,61 @@ class _ChatScreenState extends State<ChatScreen> {
   final TextEditingController _controller = TextEditingController();
 
   void _sendMessage(){
-    final user = context.read<LoginProvider>().user;
+    final user =context.read<LoginProvider>().user;
     if (user != null && _controller.text.isNotEmpty){
       FirebaseFirestore.instance.collection('chats').add({
-        'text' : _controller.text,
+        'text': _controller.text,
         'sender': user.email,
-        'timestamp': FieldValue.serverTimestamp(),
+        'timestamp':FieldValue.serverTimestamp(),
       });
       _controller.clear();
     }
   }
   @override
   Widget build(BuildContext context) {
-    User? user = context.read<LoginProvider>().user;
     return Scaffold(
-      appBar: AppBar(
-        title: Text('chat'),
-        actions: [
-          IconButton(onPressed: () async {
-            await context.read<LoginProvider>().signOut;
-            Navigator.pushNamed(context, '/login');
-          },
-              icon: Icon(Icons.logout))
-        ],
-      ),
+        appBar: AppBar(
+          title: Text('chat'),
+          actions: [IconButton(onPressed: () async {
+            await context.read<LoginProvider>().signOut();
+            Navigator.pushNamed(context, 'login');
+          }, icon: Icon(Icons.logout),)]
+        ),
       body: Column(
         children: [
-          //텍스트 출력
+          //텍스트 출력창
           Expanded(child: StreamBuilder(
             stream: FirebaseFirestore.instance
                 .collection('chats')
                 .orderBy('timestamp', descending: true)
                 .snapshots(),
             builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting){
-                return Center(child:CircularProgressIndicator(),
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(child: CircularProgressIndicator(),
                 );
               }
               final chatDocs = snapshot.data.docs;
               return ListView.builder(
-                reverse: true,
-                  itemCount: chatDocs.legth,
+                  itemCount: chatDocs.length,
                   itemBuilder: (context, index){
-                    bool isMe = chatDocs[index]['sender'] == user?.email;
-                return Row(
-                  mainAxisAlignment: isMe
-                      ? MainAxisAlignment.end
-                      : MainAxisAlignment.start,
-                  children: [
-                    Container(
-                      padding : EdgeInsets.symmetric(
-                          vertical: 10, horizontal: 15),
-                      margin: EdgeInsets.symmetric(
-                          vertical: 10, horizontal: 15),
-                      decoration: BoxDecoration(
-                        color: isMe ? Colors.grey[300] :Colors.grey[500],
-                        borderRadius:BorderRadius.all(Radius.circular(15))),
-                      child: Text(
-                        chatDocs[index]['text'],
-                        style: TextStyle(fontSize:20),
-                      ),
-                  )],
-                );
+                return Text(chatDocs[index]['text']);
               });
             },
           )),
-          //텍스트 입력
-          Padding(
-            padding: EdgeInsets.all(20),
-            child: Row(
-              children: [
-                Expanded(child: TextField(
-                  controller: _controller,
-                  decoration: InputDecoration(labelText: 'Send a message..'),
-                )),
-                IconButton(
-                    onPressed: _sendMessage, icon: Icon(Icons.send))
-              ],
-            ),
+          //텍스트 입력창
+          Padding(padding: EdgeInsets.all(20),
+          child: Row(
+            children: [
+              Expanded(
+                  child: TextField(
+                    controller: _controller,
+                decoration: InputDecoration(labelText: 'Send a message..'),
+              )),
+              IconButton(onPressed: _sendMessage, icon: Icon(Icons.send))
+            ],
+          ),
           )
-        ]
+        ],
       ),
     );
   }
